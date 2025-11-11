4ebe5a1d4ad5c97106bfe1956b4b3a84cf25aa6b
kelvinkkiprop <kelvinkiprop5@gmail.com>
2025-11-11 11:28:06 +0300

    Added serious text

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    </head>
+    <body>
+        <div style="text-align: center; padding:25px">
+            <h1>Software & AI Summit 2025</h1>
+            <p>Konza MOCK API</p>
+        </div>
+    </body>
+</html>
